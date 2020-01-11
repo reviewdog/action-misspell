684d07574e42b7dec55e7653d88375a91ec9e323
Reid Beels <reid@thedyrt.com>
2020-01-10 16:46:06 -0800

    Support a find command to select files for checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,11 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+if [ -z "$INPUT_FIND_COMMAND" ]; then
+  misspell -locale="${INPUT_LOCALE}" . \
+    | reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+else
+  eval $INPUT_FIND_COMMAND \
+    | xargs misspell -locale="${INPUT_LOCALE}" \
+    | reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+fi
