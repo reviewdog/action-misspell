5bf2d9a091b960d53a632689ea3e52251bfb48dc
Lewis Denham-Parry <lewis@control-plane.io>
2020-05-01 16:32:47 +0000

    feat: add ignore to action
    - misspell uses -i to ignore words
    - words can be comma separated

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "$GITHUB_WORKSPACE" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -error -locale="${INPUT_LOCALE}" . |
+misspell -error -locale="${INPUT_LOCALE}" -i="${INPUT_IGNORE}" . |
   reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
