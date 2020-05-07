d7b724419547034eeec89b42c997209c8bf6f357
Ichinose Shogo <shogo82148@gmail.com>
2020-05-07 22:17:53 +0900

    add path, pattern, and exclude options

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,6 @@ cd "$GITHUB_WORKSPACE" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ -z "$INPUT_FIND_COMMAND" ]; then
-  misspell -locale="${INPUT_LOCALE}" -i "${INPUT_IGNORE}" . \
+find "${INPUT_PATH:-'.'}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN:-'*'}" -print0 \
+    | xargs -0 misspell -locale="${INPUT_LOCALE}" -i "${INPUT_IGNORE}" \
     | reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
-else
-  eval $INPUT_FIND_COMMAND \
-    | xargs misspell -locale="${INPUT_LOCALE}" -i "${INPUT_IGNORE}" \
-    | reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
-fi
