951082027aba64ea58c6907b220b89a41879bb4c
Kamil Zabielski <kamil.zabielski@sysdogs.com>
2020-12-17 00:48:09 +0100

    Add input filter mode
    Signed-off-by: Kamil Zabielski <kamil.zabielski@sysdogs.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 find "${INPUT_PATH:-'.'}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN:-'*'}" -print0 \
     | xargs -0 misspell -locale="${INPUT_LOCALE}" -i "${INPUT_IGNORE}" \
-    | reviewdog -efm="%f:%l:%c: %m" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+    | reviewdog -filter-mode="${INPUT_FILTER_MODE}" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
