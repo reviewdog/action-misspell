f052121394b49daae1a5f3a5eb2f745e1a6727eb
Kamil Zabielski <kamil.zabielski@sysdogs.com>
2021-01-02 15:37:54 +0100

    Fix entrypoint
    Signed-off-by: Kamil Zabielski <kamil.zabielski@sysdogs.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 find "${INPUT_PATH:-'.'}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN:-'*'}" -print0 \
     | xargs -0 misspell -locale="${INPUT_LOCALE}" -i "${INPUT_IGNORE}" \
-    | reviewdog -filter-mode="${INPUT_FILTER_MODE:-'added'}" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+    | reviewdog -filter-mode="${INPUT_FILTER_MODE:-added}" -name="misspell" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
